f2bfbdad307c382ca8a7089e76c2e5bee41e0721
Erick Morais e Silva <erickmorais@lampp-it.com.br>
2023-10-02 18:04:25 -0300

    Dart Initial

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+=
+src/Main.dart,6\a\6a97e9b29151e1ddd8b8a9fd39f84e7c079816a0
---
+++ src/Main.dart
@@ -0,0 +1,19 @@
+void main (){
+
+  int idd = 24;
+  double altura = 1.86;
+  bool geek = (idd == altura);
+  String nome = 'Erick';
+  String frase = ('Eu sou $nome, tenho: $altura, e tenho $idd anos de idade.');
+
+  String nome1 = 'Ricardo';
+  String numero2 = "Natalia";
+  String numero3 = "Alex";
+  String numero4 = "Cristina";
+  String numero5 = "João Pedro";
+
+  List<String> listanomes = ['Ricard','Natália','Alex','Cristina','João Paulo'];
+
+  print(listanomes)[2];
+
+}
